85a8ff2c92afcbc46562b688743ec19cabfa3a68
krupalisavaliya <savaliyakrupali7@gmail.com>
2024-06-28 11:42:45 +0530

    Add files via upload

---
+++ 28-06-2024.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{   
+    int num1,num2,num3;
+    char ch1,ch2,ch3;
+    float f1,f2,f3;
+    
+    printf("\nEnter the value of num1 = ");
+    scanf("%d",&num1);
+    printf("\nEnter the value of num2 = ");
+    scanf("%d",&num2);
+    printf("\nEnter the value of num3 = ");
+    scanf("%d",&num3);
+    
+    printf("\n%d",num1);
+    printf("\n%d",num2);
+    printf("\n%d",num3);
+    
+    printf("\nEnter the value of ch1 = ");
+    scanf(" %c",&ch1);
+    printf("\nEnter the value of ch2 = ");
+    scanf(" %c",&ch2);
+    printf("\nEnter the value of ch3 = ");
+    scanf(" %c",&ch3);
+    
+    printf("\n%c",ch1);
+    printf("\n%c",ch2);
+    printf("\n%c",ch3);
+    
+    printf("\nEnter the value of f1 = ");
+    scanf(" %f",&f1);
+    printf("\nEnter the value of f2 = ");
+    scanf(" %f",&f2);
+    printf("\nEnter the value of f3 = ");
+    scanf(" %f",&f3);
+    
+    printf("\n%.2f",f1);
+    printf("\n%.3f",f2);
+    printf("\n%.4f",f3);
+    
+    
+    
+    
+    
+}
+
---
+++ demo.c
@@ -0,0 +1,24 @@
+// single line comment 
+/*
+
+       multiline comment 
+   
+   */
+   /*
+     #--->Preprocessor
+     include--->Directive
+     stdio--->standard input headed file
+     */
+     int---> "%d" or "%i"
+     longint----> "%id"
+     float----> "%f"
+     double--->"%if"
+     char---->"%c"
+        printf("hello world");
+        printf("How are you");
+     	printf("\n%d",num4);
+     	printf("\n%c",ch1);
+     	printf("\n%1f",f1);
+	 
+	 int num1=10,num2=20,num3=30,num4=40,num5=50;
+	 
